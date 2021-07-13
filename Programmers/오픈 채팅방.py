09fb46108bccd257b0f3d5aeb63a00dd0210731c
JayMaro <jinkyu1996@hanmail.net>
2021-07-13 23:12:05 +0900

    Programmers 0713

---
+++ Programmers/오픈 채팅방.py
@@ -0,0 +1,23 @@
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+name_dict = {}
+come_lst = []
+res = []
+for i in range(len(record)):
+    lst = record[i].split()
+    if lst[0] == 'Enter' or lst[0] == 'Change':
+        state, user_id, name = lst
+        name_dict[user_id] = name
+        if state == 'Enter':
+            come_lst.append((state, user_id))
+
+    else:
+        state, user_id = lst
+        come_lst.append((state, user_id))
+
+for i in range(len(come_lst)):
+    state, user_id = come_lst.pop(0)
+    if state == 'Enter':
+        res.append("{}님이 들어왔습니다.".format(name_dict[user_id]))
+    else:
+        res.append("{}님이 나갔습니다.".format(name_dict[user_id]))
+print(res)
